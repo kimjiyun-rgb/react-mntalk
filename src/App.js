bf8139f82c4a031037f4e3748e6333e9b92878a1
kimjiyun <faren02134@gmail.com>
2024-01-29 13:41:52 +0900

    123456789

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Contact from './Contact';
+import Footer from './Footer';
+import Read from './Read';
+import Comment from './Comment';
+import List from './List';
+import ReadContainer from './ReadContainer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      {/* <Contact /> */}
+      <ReadContainer />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
